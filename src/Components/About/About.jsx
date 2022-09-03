aeed658640ad0e3b08cdd53842ce1707a1740d29
Yahiaadell <imyahiaa@gmail.com>
2022-09-03 21:08:17 +0200

    Some Modifications

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
      </div>
      <hr />
      <div className="about-text">
-      CrossFit can be used to accomplish any goal, from improved health to weight loss to better performance. From general fitness to sport-specific training, there is no better program out there. The academy is located at Madianty overlooking the pool and jaccuzi, which is a phenomenal view while training.
+      CrossFit can be used to accomplish any goal, from improved health to weight loss to better performance. From general fitness to sport-specific training, there is no better program out there. CrossFit111 is located at Madianty overlooking the pool and jaccuzi, which is a phenomenal view while training.
      </div>
     </div>
   )
