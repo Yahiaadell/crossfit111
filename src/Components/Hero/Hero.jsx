20bf28e5b954fa118e828585a4b3c73c70732a7d
Yahiaadell <imyahiaa@gmail.com>
2022-09-24 00:43:07 +0200

    HeroVideo

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -9,12 +9,12 @@ const Hero = () => {
     <div className="hero" id="home">
       <div className="main-vid">
         <video className="vid-trial"src={CrossFitVid} autoPlay loop muted />
-        {/* <img src={CrossFit6} alt="" className="vid-trial" /> */}
+        
 
         <div className="main-hero">
           <Header />
           <div className="hero-title">
-            {/* <div className="hero-maint">Failure is not an option! </div> */}
+            <div className="hero-maint">Failure is not an option! </div>
             <div className="hero-buttons">
                 <Link to="join" spy={true} smooth={true}>
               <button  className="hero-btn">
