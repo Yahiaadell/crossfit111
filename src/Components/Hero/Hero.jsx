623a1df858b9e322d8db4e94c51910fdf545312b
Yahiaadell <imyahiaa@gmail.com>
2022-09-23 18:15:56 +0200

    Hero Vid

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Header from "../Header/Header";
 import "./Hero.css";
 import { Link } from "react-scroll";
-import CrossFit6 from "../../assets/CrossFit6.JPG"
+import CrossFitVid from "../../assets/CrossFitVid.mp4"
 
 const Hero = () => {
   return (
     <div className="hero" id="home">
       <div className="main-vid">
-        {/* <video className="vid-trial"src={CrossFitVid} autoPlay loop muted /> */}
-        <img src={CrossFit6} alt="" className="vid-trial" />
+        <video className="vid-trial"src={CrossFitVid} autoPlay loop muted />
+        {/* <img src={CrossFit6} alt="" className="vid-trial" /> */}
 
         <div className="main-hero">
           <Header />
