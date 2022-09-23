d411a4a5d78c472d9e4b75343bda6dcef83dd269
Yahiaadell <imyahiaa@gmail.com>
2022-09-24 01:50:35 +0200

    zoztest

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './About.css'
+import zoz from "../../assets/CrossFitVid.mp4"
 
 const About = () => {
   return (
@@ -11,6 +12,7 @@ const About = () => {
      <div className="about-text">
       CrossFit can be used to accomplish any goal, from improved health to weight loss to better performance. From general fitness to sport-specific training, there is no better program out there. CrossFit111 is located at Madianty overlooking the pool and jaccuzi, which is a phenomenal view while training.
      </div>
+     <video src={zoz} />
     </div>
   )
 }
