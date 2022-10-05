14fadb8408e2d40d67ba525ce5eda392c35e1fc2
Yahiaadell <imyahiaa@gmail.com>
2022-10-05 09:16:21 +0200

    Hero Vid

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../Header/Header";
 import "./Hero.css";
 import { Link } from "react-scroll";
-import CFvid from "../../assets/CrossFitVid.mp4"
+import CFvid from "../../assets/CrossFit1.MP4"
 
 const Hero = () => {
   return (
