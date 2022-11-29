14b0cdd250f331b503c63de1e72c2d8072c1b6ef
Yahiaadell <ym2100680@tkh.edu.eg>
2022-11-29 10:22:01 +0200

    HeroVid

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Header from "../Header/Header";
 import "./Hero.css";
 import { Link } from "react-scroll";
-import VidTrial from "../../assets/CrossFit1.MP4"
+// import VidTrial from "../../assets/CrossFit1.MP4"
 
 const Hero = () => {
   return (
     <div className="hero" id="home">
       <div className="main-vid">
-        <video className="vid-trial" autoPlay loop muted src={VidTrial}>
+        <video className="vid-trial" autoPlay loop muted>
+         <source src="https://github.com/Yahiaadell/crossfit111/raw/Production/src/assets/CrossFit1.MP4" type="video/mp4"></source>
         </video>
        
         
